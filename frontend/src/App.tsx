e59a589409d8a36a978462c7e2fcdc7c34e95173
Rajat Jamblekar <rajatdjamblekar@gmail.com>
2025-06-15 21:57:05 +0530

    initial app setup and basic code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import InputForm from "./components/InputForm";
+import GraphView from "./components/GraphView";
+
+interface Node {
+  id: string;
+}
+
+interface Link {
+  source: string;
+  target: string;
+  label: string;
+}
+
+interface GraphData {
+  nodes: Node[];
+  links: Link[];
+}
+
+function App() {
+  const [graphData, setGraphData] = useState<GraphData | null>(null);
+
+  const fetchGraph = async () => {
+    const res = await axios.get<GraphData>("http://localhost:5000/graph_data");
+    setGraphData(res.data);
+  };
+
+  useEffect(() => {
+    fetchGraph();
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Healthcare Diagnosis Network</h2>
+      <InputForm onUpdate={fetchGraph} />
+      <GraphView data={graphData} />
+    </div>
+  );
+}
+
+export default App;
